313c511628df01f0d2d6b2aa5a030e70f7cb0d55
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-02-25 14:52:42 +0000

    First Step Image uplad

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from PIL import Image, ImageColor, ImageOps
+
+# First Step - Insert Picture or will be done automatically
+a = input("Insert Photo, photo path You wish to Edit \n Insert/Paste Here: ")
+try:
+    request.urlretrieve(
+    a, "rework.png")
+    image = Image.open("rework.png")
+    print("Picture was insert")
+    image_uploaded = True
